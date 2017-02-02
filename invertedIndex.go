87ab1649b963636140cb47eeb53df033173d250e
sangclee17 <sangclee17@gmail.com>
2017-02-02 16:18:45 +1100

    Update invertedIndex.go
    summary of changes
    
    - Eliminate global variables so that using this package, separate objects can be created by NewIndex()
    - Make the code efficient as no need to compute variables during every scoring computation

--- invertedIndex.go
+++ invertedIndex.go
@@ -22,10 +22,13 @@ type Posting struct {
 	docID int
 	fdt   int
 }
-type ByScore []*Doc
 
-var invIndex map[string][]Posting
-var indexed []*Doc
+type InvIndex struct {
+	index       map[string][]Posting
+	docsIndexed []*Doc
+}
+
+type ByScore []*Doc
 
 func WriteHeader(file *os.File) {
 	fmt.Fprintf(file, "File Name, Size(words), Score\n")
@@ -50,19 +53,22 @@ func CloseFile(file *os.File) {
 	}
 }
 
-func NewIndex() {
-	invIndex = make(map[string][]Posting)
+func NewIndex() *InvIndex {
+	var inv InvIndex
+	inv.index = make(map[string][]Posting)
+	inv.docsIndexed = make([]*Doc, 0, 0)
+	return &inv
 }
 
-func IndexDocument(path string) error {
+func (inv *InvIndex) IndexDocument(path string) error {
 	file, err := os.Open(path)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	x := len(indexed)
-	indexed = append(indexed, &Doc{filepath.Base(path), 0, 0.0})
-	pdoc := indexed[x]
+	x := len(inv.docsIndexed)
+	inv.docsIndexed = append(inv.docsIndexed, &Doc{filepath.Base(path), 0, 0.0})
+	pdoc := inv.docsIndexed[x]
 
 	data, err := ioutil.ReadAll(file)
 	if err != nil {
@@ -73,73 +79,82 @@ func IndexDocument(path string) error {
 		bword := bytes.Trim(bword, ".,-~?!\"'`;:()<>[]{}\\|/=_+*&^%$#@")
 		word := string(bword)
 		lword := strings.ToLower(word)
-		list := invIndex[lword]
-		l := len(invIndex[lword])
+		list := inv.index[lword]
+		l := len(list)
 		if l > 0 && list[l-1].docID == x+1 {
 			list[l-1].fdt++
 			pdoc.size++
 			continue
 		}
-		invIndex[lword] = append(list, Posting{x + 1, 1})
+		inv.index[lword] = append(list, Posting{x + 1, 1})
 		pdoc.size++
-
 	}
 	return nil
 }
 
-func ComputeCollectionSize() int {
+func (inv *InvIndex) ComputeCollectionSize() int {
 	var totalNumberOfWords int
 
-	for _, inv := range invIndex {
-		for _, in := range inv {
-			totalNumberOfWords += in.fdt
+	for _, in := range inv.index {
+		for _, i := range in {
+			totalNumberOfWords += i.fdt
 		}
 	}
 	return totalNumberOfWords
 }
 
-func NumberOfDocuments() int {
-	return len(indexed)
+func (inv *InvIndex) NumberOfDocuments() int {
+	var numOfDocs int
+
+	for _, in := range inv.index {
+		for _, i := range in {
+			if i.docID > numOfDocs {
+				numOfDocs = i.docID
+			}
+		}
+	}
+	return numOfDocs
 }
 
-func NumOfQueryDocs(term string) int {
-	return len(invIndex[term])
+func (inv *InvIndex) NumOfQueryDocs(term string) int {
+	return len(inv.index[term])
 }
 
-func SearchTopKQuery(word string, num int) ([]*Doc, error) {
+func (inv *InvIndex) SearchTopKQuery(word string, num int) ([]*Doc, error) {
 	query := make(map[string]int)
+	collectionSize := float64(inv.ComputeCollectionSize())
+	N := float64(inv.NumberOfDocuments())
+	k1 := 1.2
+	b := 0.75
+	k3 := 100000.00
 
 	str := strings.Split(word, " ")
 	for _, val := range str {
 		(query[strings.ToLower(val)])++
 	}
+
 	for i := 0; i < len(query); i++ {
 		if num, ok := query[str[i]]; ok {
-			k1 := float64(1.2)
-			b := float64(0.75)
-			k3 := 100000.00
-			N := float64(len(indexed))
-			ft := float64(len(invIndex[str[i]]))
+			ft := float64(len(inv.index[str[i]]))
 			fqt := float64(num)
 			wqt := math.Log((N-ft+0.5)/(ft+0.5)) * (k3 + 1) * fqt / (k3 + fqt)
-			collectionSize := ComputeCollectionSize()
-			wa := float64(collectionSize / len(indexed))
-			qlist := invIndex[str[i]]
-			for i := range qlist {
-				fdt := qlist[i].fdt
-				wd := float64(indexed[qlist[i].docID-1].size)
-				Kd := k1 * ((1 - b) + b*wd/wa)
-				wdt := (k1 + 1) * float64(fdt) / (Kd + float64(fdt))
-				indexed[qlist[i].docID-1].score += wqt * wdt
+			wa := collectionSize / N
+			qlist := inv.index[str[i]]
+			for j := range qlist {
+				fdt := float64(qlist[j].fdt)
+				wd := float64(inv.docsIndexed[qlist[j].docID-1].size)
+				Kd := k1 * ((1.0 - b) + b*wd/wa)
+				wdt := (k1 + 1.0) * fdt / (Kd + fdt)
+				inv.docsIndexed[qlist[j].docID-1].score += wqt * wdt
 			}
 		}
 	}
-	sort.Sort(ByScore(indexed))
+	sort.Sort(ByScore(inv.docsIndexed))
 
 	var results []*Doc
 
 	for i := 0; i < num; i++ {
-		results = append(results, indexed[i])
+		results = append(results, inv.docsIndexed[i])
 	}
 
 	return results, nil
