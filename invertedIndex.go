2cc9fce7c518e45c10b514f2dd19ec37ce7dc7ff
sangclee17 <sangclee17@gmail.com>
2017-02-09 01:51:53 +1100

    implement Compress/DecompressIndex function

--- invertedIndex.go
+++ invertedIndex.go
@@ -92,6 +92,71 @@ func (inv *InvIndex) IndexDocument(path string) error {
 	return nil
 }
 
+func (inv *InvIndex) CompressIndex() []byte {
+	var last int
+	for i := range inv.index {
+		for j := range inv.index[i] {
+			current := inv.index[i][j].docID
+			inv.index[i][j].docID = current - last
+			last = current
+		}
+	}
+
+	compressed := make([]uint8, 0, 0)
+	for i := range inv.index {
+		for j := range inv.index[i] {
+			vByte := encodeVariant(inv.index[i][j].docID)
+			for _, v := range vByte {
+				compressed = append(compressed, v)
+			}
+		}
+	}
+	return compressed
+}
+
+func encodeVariant(num int) []uint8 {
+	output := make([]uint8, 0, 4)
+	for num > 127 {
+		output = append(output, (uint8(num)&127)|128)
+		num = num >> 7
+	}
+	output = append(output, (uint8(num))&127)
+	return output
+}
+
+func (inv *InvIndex) DecompressIndex(compressed []uint8) {
+	result := make([]int, 0, 0)
+	var temp int
+	var k int
+
+	for i := range compressed {
+		temp |= (int(compressed[i]) & 127) << (7 * uint8(k))
+		if (compressed[i] & 128) != 128 {
+			result = append(result, temp)
+			temp, k = 0, 0
+			continue
+		}
+		k++
+	}
+
+	var x int
+	for i := range inv.index {
+		for j := range inv.index[i] {
+			inv.index[i][j].docID = result[x]
+			x++
+		}
+	}
+
+	var last int
+	for i := range inv.index {
+		for j := range inv.index[i] {
+			delta := inv.index[i][j].docID
+			inv.index[i][j].docID = delta + last
+			last = inv.index[i][j].docID
+		}
+	}
+}
+
 func (inv *InvIndex) computeCollectionSize() int {
 	var totalNumberOfWords int
 
